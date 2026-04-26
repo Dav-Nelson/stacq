154571829fdb84fcae2ab9374e8b35bbe4c96434
Dav-Nelson <med.nelsondavid@gmail.com>
2026-04-26 12:31:14 +0100

    feat: setup frontend scaffolding and basic routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<div>Home</div>} />
+      <Route path="/q/:slug" element={<div>Customer Queue Page</div>} />
+      <Route path="/login" element={<div>Login</div>} />
+    </Routes>
+  </BrowserRouter>
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
